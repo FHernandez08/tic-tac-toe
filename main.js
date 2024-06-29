38c99e0ae002689ce0ccb3dadf1d991e2243a91b
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2024-06-29 12:38:02 -0500

    update of the project
    need to make adjustments for factories and IIFEs

--- main.js
+++ main.js
@@ -1,35 +1,93 @@
-const gameContainer = document.querySelector('.game-container');
-const cells = document.querySelectorAll('.cell');
-const gameStatus = document.querySelector('.game-status');
-const startBtn = document.querySelector('start-btn');
-const restartBtn = document.querySelector('restart-btn');
-const playerOne = document.querySelector('player-one-name');
-const playerTwo = document.querySelector('player-two-name');
+const statusDisplay = document.querySelector(".game-status");
 
 let gameActive = true;
-let currentPlayer = playerOne;
 
-let gameState = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
-gameStatus.innerHTML = currentPlayerTurn();
+statusDisplay.innerHTML = currentPlayerTurn();
 
-cells.forEach(cell => cell.addEventListener('click', handleCellClick));
-restartBtn.addEventListener('click', handleRestartGame);
+document
+  .querySelectorAll(".cell")
+  .forEach((cell) => cell.addEventListener("click", handleCellClick));
+document
+  .querySelector(".restart-btn")
+  .addEventListener("click", handleRestartGame);
 
 function handleCellClick(clickedCellEvent) {
-    const clickedCell = clickedCellEvent.target;
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+  const clickedCell = clickedCellEvent.target;
+  const clickedCellIndex = parseInt(
+    clickedCell.getAttribute("data-cell-index")
+  );
 
-    if (gameState[clickedCellIndex] !== '' || !gameActive) {
-        return;
-    }
+  if (gameState[clickedCellIndex] !== "" || !gameActive) {
+    return;
+  }
 
-    handleCellPlayed(clickedCell, clickedCellIndex);
-    handleResultValidation();
+  handleCellPlayed(clickedCell, clickedCellIndex);
+  handleResultValidation();
 }
 
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+  gameState[clickedCellIndex] = currentPlayer;
+  clickedCell.innerHTML = currentPlayer;
+}
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++) {
+    const winCondition = winningConditions[i];
+    let a = gameState[winCondition[0]];
+    let b = gameState[winCondition[1]];
+    let c = gameState[winCondition[2]];
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
+  }
+
+  let roundDraw = !gameState.includes("");
+  if (roundDraw) {
+    statusDisplay.innerHTML = drawMessage();
+    gameActive = false;
+    return;
+  }
+
+  handlePlayerChange();
+}
+
+function handlePlayerChange() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleRestartGame() {
+  gameActive = true;
+  currentPlayer = "X";
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  statusDisplay.innerHTML = currentPlayerTurn();
+  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+}
